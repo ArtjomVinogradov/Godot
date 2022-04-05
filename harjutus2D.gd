5770059e29b8c78799e83b98cab635bfe62b20f5
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-04-05 14:02:23 +0300

    Add files via upload

---
+++ harjutus2D.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+var playerName = "Строго без тильта парни"
+var eludearv = 0.5
+func _ready():
+	print(playerName)
+	print(eludearv)
+	print(len(playerName))
+	eludearv +=2
+	print(eludearv)
+
+var rng = RandomNumberGenerator.new()
+	print(rng.randomize())
+	var my_random_number = rng.randf_range(0.19)
